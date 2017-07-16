43e09aced61e60e6e63ef98d50fdc7c5314a4277
peterkelm <info@peterkelm.com>
2017-07-16 18:06:02 +0200

    xml sitemap generated via DOMDocument, complete list pf pages for multilingual sites

--- xml-sitemap.php
+++ xml-sitemap.php
@@ -10,9 +10,6 @@
  * @license   MIT
  */
 
-kirby()->set('snippet', 'sitemap.page', __DIR__ . '/snippets/page.php');
-kirby()->set('snippet', 'sitemap.image', __DIR__ . '/snippets/image.php');
-
 kirby()->set('route', [
     'pattern' => 'sitemap.xsl',
     'method'  => 'GET',
@@ -67,8 +64,8 @@ kirby()->set('route', [
             throw new Exception($process . ' is not callable.');
         }
 
-        $template = __DIR__ . DS . 'xml-sitemap.html.php';
-        $sitemap  = tpl::load($template, compact('languages', 'pages'));
+        $sitemap = generate_sitemap($pages, $languages);
+		// tpl::load($template, compact('languages', 'pages'));
 
         cache::set('sitemap', $sitemap);
 
@@ -109,4 +106,110 @@ function sitemapProcessAttributes($page) {
     }
 
     return $page;
+}
+
+function generate_sitemap($pages, $languages)
+{
+	// "Create" the document.
+	$xml = new DOMDocument( "1.0", "UTF-8" );
+	$xml->formatOutput = true;
+
+	$xsl = 'type="text/xsl" href="'.url('sitemap.xsl').'"';
+
+	//creating an xslt adding processing line and add it to the xml
+	$xslt = $xml->createProcessingInstruction('xml-stylesheet', $xsl);
+	$xml->appendChild($xslt);
+
+	// root node
+	$root = $xml->createElement("urlset");
+	$root->setAttribute('xmlns:image', 'http://www.google.com/schemas/sitemap-image/1.1');
+	$root->setAttribute('xmlns:xhtml', 'http://www.w3.org/1999/xhtml');
+	$root->setAttribute('xmlns', 'http://www.sitemaps.org/schemas/sitemap/0.9');
+	$xml->appendChild($root);
+
+	// copy languages codes to separate array to not disturb the iteration below
+	$sitelangs = array();
+	foreach ($languages as $lang) {
+		array_push($sitelangs, $lang->code());
+	}
+
+	// "crawl" page tree
+	foreach ($pages as $page) {
+		// iterate over site languages
+		foreach($sitelangs as $langs) {
+			// list only if the content file exists
+			if ($page->content($langs)->exists()) {
+				$root->appendChild(add_sitemap_url($xml, $page, $languages, $langs));
+			}
+		}
+	}
+
+	return $xml->saveXML();
+}
+
+// $language can be null if we just want the default language
+function add_sitemap_url($xml, $page, $languages, $language = null){
+
+	$url = $xml->createElement("url");
+
+	$loc = $xml->createElement("loc", html($page->url($language)));
+	$url->appendChild($loc);
+
+	/* <lastmod><?= date('c', $page->modified()) ?></lastmod>
+	   20170702 pk --- this did not work correctly for dates of blog posts */
+	$lastmod = $xml->createElement("lastmod", $page->date() ? $page->date('c') : $page->modified('c'));
+	$url->appendChild($lastmod);
+
+	if ($languages && $languages->count() > 1) {
+		foreach ($languages as $lang) {
+			// list only if the content file exists
+			if ($page->content($lang->code())->exists()) {
+				$langs = $xml->createElement("xhtml:link");
+				$langs->setAttribute('hreflang', $lang->code());
+				$langs->setAttribute('href', html($page->url($lang->code())));
+				$langs->setAttribute('rel', "alternate");
+				$url->appendChild($langs);
+			}
+		}
+	}
+
+	if (c::get('sitemap.priority', false)) {
+		$prio = $xml->createElement("priority", $page->priority());
+		$url->appendChild($prio);
+	}
+
+	if (c::get('sitemap.frequency', false)) {
+		$freq = $xml->createElement("changefreq", $page->frequency());
+		$url->appendChild($freq);
+	}
+
+	// images
+	if (c::get('sitemap.include.images', true) && $page->hasImages()) {
+		foreach ($page->images() as $image) {
+			$img = $xml->createElement("image:image");
+			$url->appendChild($img);
+
+			$loc = $xml->createElement("image:loc", html($image->url()));
+			$img->appendChild($loc);
+
+			// caption is language specific
+			$meta = $image->meta($language);
+			
+			if ($meta->caption()->isNotEmpty() || $meta->alt()->isNotEmpty()) {
+				$caption = $meta->caption()->isNotEmpty() ? $meta->caption() : $meta->alt();
+
+				$cap = $xml->createElement("image:caption");
+				$cdata = $xml->createCDATASection($caption);
+				$cap->appendChild($cdata);
+				$img->appendChild($cap);
+			}
+
+			if ($license = c::get('sitemap.images.license', null)) {
+				$lic = $xml->createElement("image:license", html($license));
+				$img->appendChild($lic);
+			}
+		}
+	}
+
+	return $url;
 }
